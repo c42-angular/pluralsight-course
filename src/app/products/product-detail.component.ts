25eb7bfe87632e2a062845d050cdb917309ea76c
Arunkumar Ramachandriya <arun@arunkumars-mbp.home>
2019-02-26 12:57:32 +0000

    routerLink navigation

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { IProduct } from './product';
 
@@ -11,9 +12,23 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+
+    this.pageTitle += `: ${id}`;
+
+    this.product =   {
+      productId: 2,
+      productName: 'Garden Cart',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2016',
+      description: '15 gallon capacity rolling garden cart',
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl: 'https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
+    };
   }
 
 }
